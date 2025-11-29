d502f4657382bcbab99014ec1a0b1cd38065d329
pavan <pavankumarseepana1210@gmail.com>
2025-11-30 00:41:22 +0530

    config: use FRONTEND_URL env var for cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -12,7 +12,9 @@ connectDB();
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: process.env.FRONTEND_URL || '*',
+}));
 app.use(helmet());
 app.use(morgan('dev'));
 
